9ccc37412651729166b381df998e09596a754254
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2017-10-19 18:17:54 +0200

    standard tabs

--- elisp/custom-editing.el
+++ elisp/custom-editing.el
@@ -1,7 +1,7 @@
 ;; tab width
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 8)
-
+(defvaralias 'c-basic-offset 'tab-width)
 ;; max. 80 char 
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 (setq-default fill-column 80)
