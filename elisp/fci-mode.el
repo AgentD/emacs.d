959603fc137cb9caaa01ecf462f7ba99ec88aeb9
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2017-11-12 22:23:56 +0100

    minor changes

--- elisp/fci-mode.el
+++ elisp/fci-mode.el
@@ -1,6 +1,6 @@
 
 ;; 80 char. indicator
-(setq fci-rule-width 5)
+(setq fci-rule-width 4)
 ;;(setq fci-rule-color "#fb0117")
 (setq fci-rule-color "#b0b0b0")
 (global-set-key (kbd "<f12>") 'fci-mode)
--- init.el
+++ init.el
@@ -21,7 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "simp" :slant normal :weight normal :height 90 :width normal)))))
+ '(default ((t (:family "xos4 Terminus" :foundry "xos4" :slant normal :weight normal :height 90 :width normal)))))
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
