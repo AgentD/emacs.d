6af73723d395a287d4b02ccd108968ff011d40b4
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2017-11-23 21:32:56 +0100

    minor changes

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (flx-ido markdown-mode auto-complete-auctex jdee haskell-mode fill-column-indicator auto-complete auctex)))
+    (yasnippet flx-ido markdown-mode auto-complete-auctex jdee haskell-mode fill-column-indicator auto-complete auctex)))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(size-indication-mode t)
