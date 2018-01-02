233c1a6c4e91ef6325c3b45afde73c14c18e0578
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-01-02 13:02:24 +0100

    minor changes

--- elisp/custom-keys.el
+++ elisp/custom-keys.el
@@ -1,9 +1,9 @@
 ;; Custom keybinds
-(global-set-key (kbd "C-h") 'backward-delete-char)
+(global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "C-w") 'backward-kill-sexp)
-
 (global-set-key (kbd "M-p") 'backward-paragraph)
 (global-set-key (kbd "M-n") 'forward-paragraph)
+
 (global-set-key (kbd "<print>") 'menu-bar-open) 
 (global-set-key (kbd "<f11>") 'linum-mode)
 (global-set-key (kbd "<f9>") 'font-lock-mode)
@@ -14,6 +14,7 @@
 (global-set-key (kbd "M-<next>") 'next-buffer)
 (global-set-key (kbd "C-<next>") 'next-buffer)
 (global-set-key (kbd "<f5>") 'previous-buffer)
+(global-set-key (kbd "M-<prior>") 'previous-buffer)
 (global-set-key (kbd "C-<prior>") 'previous-buffer)
 
 ;;window resizing
@@ -22,11 +23,15 @@
 (global-set-key (kbd "<s-down>") 'shrink-window)
 (global-set-key (kbd "<s-right>") 'enlarge-window-horizontally)
 
-(global-set-key (kbd "M-k") 'compile)
+(global-set-key (kbd "s-k") 'compile)
 (global-set-key (kbd "<f8>") 'compile)
+(global-set-key (kbd "s-S-k") 'recompile)
+(global-set-key (kbd "M-<f8>") 'recompile)
+(global-set-key (kbd "C-<f8>") 'kill-compilation)
+(global-set-key (kbd "s-C-k") 'kill-compilation)
 
-(global-set-key (kbd "<f7>") 'org-latex-export-to-pdf)
-(global-set-key (kbd "C-c <print>") 'org-latex-export-to-pdf)
-(global-set-key (kbd "C-<print>") 'org-latex-export-to-pdf)
-(global-set-key (kbd "M-<print>") 'org-latex-export-to-pdf)
+(add-hook 'org-mode-hook (lambda()
+(local-set-key (kbd "<f7>") 'org-latex-export-to-pdf)
+	(local-set-key (kbd "C-c <print>") 'org-latex-export-to-pdf)
+	(local-set-key (kbd "M-<print>") 'org-latex-export-to-pdf)))
 
