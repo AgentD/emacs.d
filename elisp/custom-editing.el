d60ff7ae0fb8f654c074be8ddc95cf30519501e3
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-01-06 11:38:48 +0100

    added lkcs

--- elisp/custom-editing.el
+++ elisp/custom-editing.el
@@ -2,6 +2,8 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 8)
 (defvaralias 'c-basic-offset 'tab-width)
+(setq c-default-style "Linux" 
+      c-basic-offset 8)
 (add-hook 'java-mode-hook (lambda()
 			    	(setq c-basic-offset 4)))
 ;; max. 80 char 
