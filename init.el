ecd00721db1e9f6cbc256a5e439a87a48daa94c4
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-01-17 00:03:37 +0100

    minor changes

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-;; '(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight normal :height 90 :width normal)))))
+ ;;'(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight normal :height 71 :width normal)))))
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
