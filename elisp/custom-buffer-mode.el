128e2c66d3b2de64addcd1d083431ef7d1dd9e85
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-01-18 12:23:46 +0100

    added

---
+++ elisp/custom-buffer-mode.el
@@ -0,0 +1,11 @@
+;; Forces the messages to 0, and kills the *Messages* buffer - thus disabling it on startup.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
+;; Disabled *Completions*
+(add-hook 'minibuffer-exit-hook 
+          '(lambda ()
+             (let ((buffer "*Completions*"))
+               (and (get-buffer buffer)
+                    (kill-buffer buffer)))))	
+
