39171f573ca60f482ac2873662d9d1de5998d22c
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-01-18 13:02:24 +0100

    removed removal of *scratch* due to complicatons with ido-mode

--- elisp/custom-buffer-mode.el
+++ elisp/custom-buffer-mode.el
@@ -6,14 +6,7 @@
 (add-hook 'minibuffer-exit-hook 
           '(lambda ()
              (let ((buffer "*Completions*"))
-               (and (get-buffer buffer)
+              (and (get-buffer buffer)
                     (kill-buffer buffer)))))	
 
-;; Makes *scratch* empty.
-;; (setq initial-scratch-message "")
 
-;; Removes *scratch* from buffer after the mode has been set.
-(defun remove-scratch-buffer ()
-  (if (get-buffer "*scratch*")
-      (kill-buffer "*scratch*")))
-(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
