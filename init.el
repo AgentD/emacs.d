85d7161fffd7639dcf70d1067ddb5d32455bf273
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-01-19 22:15:26 +0100

    changes to reduce startup time of terminal only mode; added magit

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (yasnippet flx-ido markdown-mode auto-complete-auctex jdee haskell-mode fill-column-indicator auto-complete auctex)))
+    (magit yasnippet flx-ido markdown-mode auto-complete-auctex jdee haskell-mode fill-column-indicator auto-complete auctex)))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(size-indication-mode t)
@@ -27,33 +27,36 @@
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(package-initialize)
-
-
 ;; custom color-theme
 (add-to-list 'custom-theme-load-path
-			              (file-name-as-directory "~/.emacs.d/themes"))
+(file-name-as-directory "~/.emacs.d/themes"))
 (load-theme 'gl-dark t)
-
 (enable-theme 'gl-dark)
+
+(when (display-graphic-p)
+	(require 'package)
+	(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+	(package-initialize)
+	;;(load-library "evil-mode-custom") 
+	(load-library "snippets")
+	(load-library "emacs-goodies-el"))
 
 ;; better find-file
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 (ido-everywhere t)
 
-;; custom evil mode
-;;(load-library "evil-mode-custom") 
+
 (load-library "custom-buffer-mode")
+(load-library "custom-editing")
 (load-library "custom-keys")
 (load-library "fci-mode")
-(load-library "custom-editing")
-(load-library "snippets")
-;; load emacs goodies 
-(load-library "emacs-goodies-el")
-
 ;; manage backups/autosaves
 (load-library "backup-autosave")
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
