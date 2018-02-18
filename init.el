51ba53ffe9fcb3578910507f349e4b1558498b1f
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-02-18 22:38:35 +0100

    minor changes

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
  '(display-battery-mode t)
@@ -51,3 +52,9 @@
 ;; manage backups/autosaves
 (load-library "backup-autosave")
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
