d22b29c535c881f0710394c75ec7de2b061b7171
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-02-28 10:11:35 +0100

    minor changes

--- init.el
+++ init.el
@@ -13,18 +13,19 @@
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (magit yasnippet flx-ido auto-complete-auctex jdee haskell-mode fill-column-indicator auto-complete auctex)))
- '(scroll-bar-mode nil)
+    (undo-tree magit yasnippet flx-ido auto-complete-auctex jdee haskell-mode fill-column-indicator auto-complete auctex)))
+ '(scroll-bar-mode (quote right))
  '(show-paren-mode t)
  '(size-indication-mode t)
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
-;;(custom-set-faces
+(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  ;;'(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight normal :height 71 :width normal)))))
+ '(scroll-bar ((t (:background "grey9" :distant-foreground "grey9" :foreground "gray9")))))
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
@@ -32,7 +33,6 @@
 (add-to-list 'custom-theme-load-path (file-name-as-directory "~/.emacs.d/themes"))
 (load-theme 'gl-dark t)
 (enable-theme 'gl-dark)
-
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -44,7 +44,7 @@
 (ido-everywhere t)
 
 (load-library "snippets")
-(load-library "emacs-goodies-el")
+;;(load-library "emacs-goodies-el")
 (load-library "custom-buffer-mode")
 (load-library "custom-editing")
 (load-library "custom-keys")
@@ -52,9 +52,3 @@
 ;; manage backups/autosaves
 (load-library "backup-autosave")
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
