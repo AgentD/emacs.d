a7fb2c420c4f1886c068c8b11d28396154fd108a
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-03-12 09:33:32 +0100

    changed theme to gl-dark-alt

--- init.el
+++ init.el
@@ -20,14 +20,18 @@
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(scroll-bar ((t (:background "grey9" :distant-foreground "grey9" :foreground "gray9")))))
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
 ;; custom color-theme
 (add-to-list 'custom-theme-load-path (file-name-as-directory "~/.emacs.d/themes"))
-(load-theme 'gl-dark t)
-(enable-theme 'gl-dark)
+(load-theme 'gl-dark-alt t)
+(enable-theme 'gl-dark-alt)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -42,6 +46,7 @@
 (load-library "snippets")
 ;;(load-library "emacs-goodies-el")
 (load-library "custom-buffer-mode")
+(load-library "custom-terminal-mode")
 (load-library "custom-editing")
 (load-library "custom-keys")
 (load-library "fci-mode")
