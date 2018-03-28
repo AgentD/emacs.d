33884a3ad1d52110ec61abc06b815dd1df9df1c7
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-03-28 14:53:20 +0200

    initial commit

---
+++ themes/gl-term-alt-theme.el
@@ -0,0 +1,46 @@
+(deftheme gl-term-alt
+  "gl-term-alt theme")
+
+(custom-theme-set-faces
+ 'gl-term
+ 
+ '(default ((t (:background "black" :foreground "#ffffff"))))
+ '(mouse ((t (:foreground "black"))))
+ '(cursor ((t (:background "#114488"))))
+ '(border ((t (:foreground "blue"))))
+ '(fringe ((t (:background "#151515"))))
+ '(mode-line ((t (:foreground "#000000" :background "#bcbcbc" ))))
+ '(mode-line-buffer-id ((t (:foreground "#000000":background nil))))
+ '(menu ((t (:background "#bcbcbc" :foreground "black"))))
+ '(region ((t (:background "#114488" :foreground "#ffffff"))))
+ '(font-lock-builtin-face ((t (:foreground "#75507b"))))
+ '(font-lock-comment-face ((t (:foreground "#cc0000"))))
+ '(font-lock-constant-face ((t (:foreground "#d315c3"))))
+ '(font-lock-function-name-face ((t (:foreground "#75507b"))))
+ '(font-lock-keyword-face ((t (:foreground "#729fcf"))))
+ '(font-lock-string-face ((t (:foreground "#4e9a06"))))
+ '(font-lock-type-face ((t (:foreground "#4e9a06"))))
+ '(font-lock-variable-name-face ((t (:foreground "#c4a000"))))
+ '(minibuffer-prompt ((t (:foreground "#ffffff" :bold t))))
+ '(font-lock-warning-face ((t (:foreground "Red" :bold t))))
+ '(linum ((t (:foreground "#585858" :background "#000000" :underline nil))))	
+
+ 
+ '(ido-subdir ((t (:foreground "#a1c659")))) 
+ '(ido-first-match ((t (:foreground "#ffffff" :bold t :underline t))))
+ '(ido-only-match ((t (:foreground "#ffcc33")))) 
+ '(ido-indicator ((t (:foreground "#ffffff")))) 
+ '(ido-incomplete-regexp ((t (:foreground "#ffffff")))) 
+
+ '(diff-added ((t (:foreground "green"))))
+ '(diff-removed ((t (:foreground "red"))))
+ '(diff-header ((t (nil))))
+ )
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'gl-term-alt)
+
