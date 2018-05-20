f29a9ba32f444b910c353b929770e5efb1ab86fe
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-05-20 21:53:37 +0200

    updated

--- init.el
+++ init.el
@@ -1,4 +1,8 @@
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
@@ -6,6 +10,9 @@
  '(display-time-mode t)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+	(yasnippet visual-fill-column undo-tree s memoize markdown-mode magit haskell-mode goto-chg flycheck fill-column-indicator)))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(size-indication-mode t)
@@ -43,3 +50,9 @@
 (load-library "fci-mode")
 ;; manage backups/autosaves
 (load-library "backup-autosave")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
