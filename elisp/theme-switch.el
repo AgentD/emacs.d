a6b1d823db55c19e2b62174d6178fda9af26b6f8
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-06-01 16:47:15 +0200

    initial commit

---
+++ elisp/theme-switch.el
@@ -0,0 +1,24 @@
+(setq custom-themes '(gl-dark-alt xcode-dark gl-light))
+(setq custom-themes-index 0)
+
+(defun custom-cycle-theme ()
+  (interactive)
+  (setq custom-themes-index (% (1+ custom-themes-index) (length custom-themes)))
+  (custom-load-indexed-theme))
+
+(defun custom-load-indexed-theme ()
+  (custom-try-load-theme (nth custom-themes-index custom-themes)))
+
+(defun custom-default-theme ()
+  (interactive)
+  (setq custom-themes-index 0)
+  (custom-load-indexed-theme))
+
+(defun custom-try-load-theme (theme)
+  (if (ignore-errors (load-theme theme :no-confirm))
+      (mapcar #'disable-theme (remove theme custom-enabled-themes))
+    (message "Unable to find theme file for ‘%s’" theme)))
+
+
+(global-set-key (kbd "<f2>") 'custom-cycle-theme)
+(global-set-key (kbd "C-<f2>") 'custom-default-theme)
