4a169f611ecb50f24a6a1fba4815b91c666a44d9
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-06-16 18:25:40 +0200

    updated

--- root_emacs.d/elisp/backup-autosave.el
+++
@@ -1,5 +0,0 @@
-(defvar backup-dir (expand-file-name "~/.emacs.d/backup/"))
-(defvar autosave-dir (expand-file-name "~/.emacs.d/autosave/"))
-			
-(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosave/" t)))
