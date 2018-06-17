cb9cca88883df7cecb20e7baec93a2806ab75501
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-06-17 23:26:17 +0200

    updated

--- init.el
+++ init.el
@@ -33,8 +33,6 @@
 									  "~/.emacs.d/themes"))
 (load-theme 'gl-dark-alt t)
 (enable-theme 'gl-dark-alt)
-;;(load-theme 'xcode-dark t)
-;;(enable-theme 'xcode-dark)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
