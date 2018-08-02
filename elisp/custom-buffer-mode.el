4d41392669ed8b5113e7dcff2f16a5a883948a5c
David Oberhollenzer <david.oberhollenzer@tele2.at>
2018-08-02 21:27:04 +0200

    Disable messages buffer
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- elisp/custom-buffer-mode.el
+++ elisp/custom-buffer-mode.el
@@ -1,3 +1,8 @@
+;; Forces the messages to 0, and kills the *Messages* buffer
+;; thus disabling it on startup.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
 ;; Disabled *Completions* Buffer
 (add-hook 'minibuffer-exit-hook
           '(lambda ()
