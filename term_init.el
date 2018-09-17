6f6a7c55b90133839d272dd81775e5a362886466
Gabriel Lanziner <gabriel.lanziner@profisoftware.com>
2018-09-17 11:48:27 +0200

    removed gui settings within term_init.el (subsequently removed memacs-theme.el as well)

--- term_init.el
+++ term_init.el
@@ -15,26 +15,11 @@
  '(trailing-whitespace ((t (:background "#ffb6b0"))))
  '(minibuffer-prompt ((t (:inherit foreground))))
  '(mode-line-buffer-id ((t (:inherit background))))
- '(scroll-bar ((t (:background "#454545" :distant-foreground "#454545" :foreground "#454545"))))
  '(dired-directory ((t (:foreground "#75507b"))))
 ;; '(default ((t (:family "Monospace" :foundry "xft" :slant normal :weight
  ;; normal :height 69 :width normal))))
  )
 (add-to-list 'load-path "~/.emacs.d/elisp")
-(when (display-graphic-p)
-    (progn
-		(require 'package)
-		(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-		(package-initialize)
-		(setq frame-title-format '("" "emacs@" system-name " - %b"))
-		(add-to-list 'custom-theme-load-path (file-name-as-directory
-											  "~/.emacs.d/themes"))
-		(load-theme 'memacs t)
-		(enable-theme 'memacs)
-		(load-library "custom-buffer-mode")
-		(load-library "custom-terminal-mode")
-		(load-library "custom-dired-mode"))
-	)
 
 (load-library "custom-editing")
 (load-library "custom-keys")
